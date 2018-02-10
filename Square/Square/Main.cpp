4a196776d1ea4c1ff0fd532fdbcb1a2ecb602929
stackprobe <stackprobes@gmail.com>
2018-02-11 00:23:48 +0900

    daily

--- Square/Square/Main.cpp
+++ Square/Square/Main.cpp
@@ -75,6 +75,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 //	SetFullSceneAntiAliasingMode(4, 2); // 適当な値が分からん。フルスクリーン廃止したので不要
 
+	SetWindowIconID(333);
+
 	errorCase(DxLib_Init()); // ? 失敗
 
 	DxLibInited = 1;
